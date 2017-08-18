78365b7a50e16f2da7675a037bb83e4aba501b5e
Paul Gebheim <pgebheim@users.noreply.github.com>
2017-08-17 18:15:59 -0700

    Updating indexer.js

--- indexer.js
+++ indexer.js
@@ -1,6 +1,7 @@
-var algoliasearch = require('algoliasearch@3.10.2');
+const algoliasearch = require('algoliasearch@3.10.2');
+const _ = require('lodash@4.8.2');
 
-const indexMessage = function(event, cb) {
+const indexMessage = function(index, event, cb) {
   index.addObject(event, event.event_id, function(err, content) {
     if(err) {
       cb(err);
@@ -10,17 +11,33 @@ const indexMessage = function(event, cb) {
   });
 }
 
-const findMessages = function(terms, cb) {
+const responseTemplate = _.template("You wanted history? We found <%= count %> in <%= processingTime %>ms!");
+const findMessages = function(index, terms, cb) {
+  index.search(terms, {hitsPerPage: 5, attributesToHighlight: ['text']}, function searchDone(err, content) {
+    if (err) {
+      return cb(err);
+    }
   
+    console.log(content);
+    cb(null, {
+      text: responseTemplate({count: content.nbHits, processingTime: content.processingTimeMS}),
+      attachments: _.map(content.hits, function(hit) {
+        return {
+          text: hit.text
+        };
+      })
+    });
+  });
 }
 
 module.exports = function (context, cb) {
   var client = algoliasearch(context.secrets.ALGOLIA_APP_ID, context.secrets.ALGOLIA_API_KEY);
   var index = client.initIndex('auth0_slack_messages');
+  var event = context.data.event || {};
   
-  if(context.data.event.type == "message") {
-    indexMessage(context.data.event);
-  } else {
-    console.log(context);
+  if(event.type == "message") {
+    indexMessage(index, event, cb);
+  } else if (context.data.command == '/history') {
+    findMessages(index, context.data.text, cb)
   }
 }
